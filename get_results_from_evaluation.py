32038c936c7c4b5b0d417b8d40c272f9286e3bb7
Marcelo Martins <rezende.martins@gmail.com>
2020-05-10 18:02:08 -0300

    changing to plot top5 and top10 results

--- get_results_from_evaluation.py
+++ get_results_from_evaluation.py
@@ -9,6 +9,9 @@ class Result:
     top1_std = 0
     mrr = 0
     mrr_std = 0
+    top5 = None
+    top10 = None
+    eval_code_length = None
     def __init__(self, full_model_name, model_name, filters, kernel_size, margin):
         self.full_model_name = full_model_name
         self.model_name = model_name
@@ -25,13 +28,31 @@ def get_decimal(line):
     else:
         return ''
 
+def calculate_top5(positions):
+    count = 0
+    for pos in positions:
+        if pos <= 5:
+            count += 1
+
+    return count / len(positions)
+
+def calculate_top10(positions):
+    count = 0
+    for pos in positions:
+        if pos <= 10:
+            count += 1
+
+    return count / len(positions)
+
 results = []
 model_name_regx = re.compile('((([a-z]{2,9}-)+)([0-9]{2,4}))-(k-[0-9])?-?(m-[0-9]{2,3})?-?out\.txt')
-mypath = 'output/evaluation/best/'
+mypath = 'output/evaluation/code-length/'
 filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 top1_line = 'Top1 Description'
 mrr_line = 'MRR Description'
+final_results_desc = 'INFO: final_results:'
+eval_desc = 'eval: '
 
 default_kernel_size = '1,2,3,5,7'
 default_margin = '0009'
@@ -59,10 +80,12 @@ for filename in filenames:
     else:
         margin = margin_match.split('-')[1]
 
-    result = Result(full_model_name, model_name, filters, kernel_size, margin)
+    
+    
 
     with open(join(mypath, filename), 'r') as f:
         line = f.readline()
+        result = None
         while line:
             if top1_line in line or mrr_line in line:
 
@@ -80,30 +103,67 @@ for filename in filenames:
                     result.mrr_std = std
 
                 line = f.readline()
+            elif final_results_desc in line:
+                aux = line
+                open_curly_braces_pos = aux.find('{')
+                final_results = eval(aux[open_curly_braces_pos:])
+                positions = [pos for i in final_results['positions'] for pos in i]
+
+                result = Result(full_model_name, model_name, filters, kernel_size, margin)
+
+                result.top5 = calculate_top5(positions)
+                result.top10 = calculate_top10(positions)
+
+                results.append(result)
+
+                line = f.readline()
+            elif eval_desc in line:
+                aux = line
+                start_pos = aux.find('eval_')
+                end_pos = aux.find('.json')
+                eval_code_length = aux[start_pos: end_pos]
+
+                line = f.readline()
+
+                aux = line
+                open_curly_braces_pos = aux.find('{')
+                final_results = eval(aux[open_curly_braces_pos:])
+                positions = [pos for i in final_results['positions'] for pos in i]
+
+                result = Result(full_model_name, model_name, filters, kernel_size, margin)
+
+                result.top5 = calculate_top5(positions)
+                result.top10 = calculate_top10(positions)
+                result.eval_code_length = eval_code_length
+
+                results.append(result)
+
+                line = f.readline()
+
             else:
                 line = f.readline()
 
-    results.append(result)
+    
 
 
 sorted_results = sorted(results, key=attrgetter('model_name', 'filters'))
 
-for k, g in groupby(sorted_results, lambda m: m.full_model_name):
+for k, g in groupby(sorted_results, lambda m: m.full_model_name if m.eval_code_length is None else f'{m.full_model_name} - {m.eval_code_length}'):
     count = 0
     line = ''
     for i in sorted(g, key=attrgetter('kernel', 'margin')):
         if count == 0:
-            line += f'{i.full_model_name}, '
+            line += f'{i.full_model_name}, ' if i.eval_code_length is None else f'{i.full_model_name} - {i.eval_code_length},'
             if i.kernel != default_kernel_size:
-                line += f'X,X,{i.mrr} +- {i.mrr_std}, {i.top1} +- {i.top1_std},'
+                line += f'X,X,X,X,{i.mrr} +- {i.mrr_std}, {i.top1} +- {i.top1_std},{i.top5}, {i.top10},'
                 count += 1
             else:
-                line += f'{i.mrr} +- {i.mrr_std}, {i.top1} +- {i.top1_std},'
+                line += f'{i.mrr} +- {i.mrr_std}, {i.top1} +- {i.top1_std},{i.top5}, {i.top10},'
         else:
-            line += f'{i.mrr} +- {i.mrr_std}, {i.top1} +- {i.top1_std},'
+            line += f'{i.mrr} +- {i.mrr_std}, {i.top1} +- {i.top1_std},{i.top5}, {i.top10},'
 
         count += 1
     while count < 7:
-        line += f'X,X,'
+        line += f'X,X,X,X,'
         count += 1
     print(line)
